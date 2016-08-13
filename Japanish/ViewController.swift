a4f5d8539814c3ae0c1a32b5cd4ef8844e1ad7a9
hashimotog <hashimotog@isao.co.jp>
2016-08-13 15:17:05 +0900

    Initial Commit

---
+++ Japanish/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Japanish
+//
+//  Created by 橋本　豪 on 2016/08/13.
+//  Copyright © 2016年 go.hashimoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
